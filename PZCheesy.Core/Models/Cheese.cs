996d022f9b36be29fc8f810d5606f1ed97235d74
bschirmer <bmschirmer@gmail.com>
2020-02-19 11:30:25 +1000

    Added unit tests project; Started adding Core logic interfaces

---
+++ PZCheesy.Core/Models/Cheese.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PZCheesy.Core.Models
+{
+    public class Cheese
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Colour { get; set; }
+        public float Price { get; set; }
+        public string PictureRef { get; set; }
+
+    }
+}
---
+++ PZCheesy.Core/Services/ICheeseService.cs
@@ -0,0 +1,14 @@
+﻿using PZCheesy.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZCheesy.Core.Services
+{
+    public interface ICheeseService
+    {
+        Task<IEnumerable<Cheese>> GetAllCheeseAsync();
+
+    }
+}
